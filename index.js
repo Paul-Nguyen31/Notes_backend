03316d083ebff141fc48fa665bcf6b2ae1085f20
Paul Nguyen <Paulhoang31@gmail.com>
2023-04-26 12:27:03 -0500

    added Build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 
 
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -19,6 +20,8 @@ const unknownEndpoint = (request, response) => {
 app.use(requestLogger)
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+
 
 let notes = [
     {
@@ -105,7 +108,7 @@ console.log("this is ", note)
 // })
 
 app.use(unknownEndpoint)
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
